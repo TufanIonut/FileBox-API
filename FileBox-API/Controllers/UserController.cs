d80ae7a83468cf4e96d95813e5dc2184fd4ed91a
Tufan Ionut-Petrisor <117408976+TufanIonut@users.noreply.github.com>
2024-07-28 21:42:49 +0300

    initialize the project

---
+++ FileBox-API/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FileBox_API.Controllers
+{
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/user")]
+        public IActionResult Get()
+        {
+            return Ok("Hello from UserController");
+        }
+    }
+}
